a055c175c51d96cbb5f1c08519bd4c81f6cb1029
INDYWIN <111363345+INDYWIN@users.noreply.github.com>
2022-08-22 16:15:56 +0700

    New002

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$conn_1 = mysqli_connect("database-1.cc45kymlat6n.ap-southeast-1.rds.amazonaws.com","admin","","beauti_project");
+$conn_1 = mysqli_connect("Localhost","root","","beauti_project");
 
 ?>
